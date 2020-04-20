37fc322aedd250b1b5d4153d0f10498a74fbcf25
Nacy Mulligan <43332601+wwwwwmin@users.noreply.github.com>
2020-04-20 10:52:20 +0800

    分析全部依赖graphInfo_1

--- bundler.js
+++ bundler.js
@@ -29,8 +29,8 @@ const moduleAnalyser = (filename) => {
     });
     console.log(dependencies);
     //将抽象语法树转化成浏览器可运行的代码
-    const {code} = babel.transformFromAst(ast,null,{
-        presets:["@babel/preset-env"]
+    const { code } = babel.transformFromAst(ast, null, {
+        presets: ["@babel/preset-env"]
     });
     console.log(code);
     return {
@@ -40,5 +40,37 @@ const moduleAnalyser = (filename) => {
     }
 }
 
-const moduleInfor =  moduleAnalyser('./src/index.js');
-console.log(moduleInfor);
+
+const makeDependenciesGraph = (entry) => {
+    const entryModule = moduleAnalyser(entry);
+    // console.log(entryModule);
+    const graphArray = [entryModule];
+    for (let i = 0; i < graphArray.length; i++) {
+        const item = graphArray[i];
+        const { dependencies } = item;
+        //若文件有依赖，还要对依赖文件进行分析
+        if (dependencies) {
+            for (let j in dependencies) {
+                graphArray.push(
+                    moduleAnalyser(dependencies[j])
+                );
+            }
+        }
+    }
+    console.log(graphArray);
+    //数据格式转换(数组-->对象)
+    const graph = {};
+    graphArray.forEach(item => {
+        graph[item.filename] = {
+            dependencies: item.dependencies,
+            code: item.code
+        }
+    })
+    return graph;
+}
+
+// const moduleInfor =  moduleAnalyser('./src/index.js');
+// console.log(moduleInfor);
+
+const graphInfo = makeDependenciesGraph('./src/index.js');
+console.log(graphInfo);
